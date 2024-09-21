29ecc671c36f15964952663b137db5312a538fda
Danny Zambrano11 <dzambrano7038@utm.edu.ec>
2024-09-21 12:25:26 -0500

    flores amarillas

---
+++ flores-amarillas-tres-main/anim.js
@@ -0,0 +1,53 @@
+// Sincronizar las letras con la canción
+var audio = document.querySelector("audio");
+var lyrics = document.querySelector("#lyrics");
+
+// Array de objetos que contiene cada línea y su tiempo de aparición en segundos
+var lyricsData = [
+  { text: "En cada paso que doy, en cada risa y en cada lágrima, te encuentro a mi lado. ", time: 1 },
+  { text: "Gracias por estar conmigo en los días brillantes", time: 3 },
+  { text: "y en los más oscuros, por ser mi apoyo constante", time: 6 },
+  { text: "y por recordarme siempre cómo sonreír,", time: 10 },
+  { text: "incluso cuando el mundo se pone difícil.", time: 15 },
+  { text: "Te quiero más de lo que las palabras pueden expresar", time: 20 },
+  { text: "porque has sido mi refugio y mi alegría en todo momento.", time: 25 },
+  { text: "Love.", time: 140 },
+];
+
+// Animar las letras
+function updateLyrics() {
+  var time = Math.floor(audio.currentTime);
+  var currentLine = lyricsData.find(
+    (line) => time >= line.time && time < line.time + 6
+  );
+
+  if (currentLine) {
+    // Calcula la opacidad basada en el tiempo en la línea actual
+    var fadeInDuration = 0.1; // Duración del efecto de aparición en segundos
+    var opacity = Math.min(1, (time - currentLine.time) / fadeInDuration);
+
+    // Aplica el efecto de aparición
+    lyrics.style.opacity = opacity;
+    lyrics.innerHTML = currentLine.text;
+  } else {
+    // Restablece la opacidad y el contenido si no hay una línea actual
+    lyrics.style.opacity = 0;
+    lyrics.innerHTML = "";
+  }
+}
+
+setInterval(updateLyrics, 1000);
+
+//funcion titulo
+// Función para ocultar el título después de 216 segundos
+function ocultarTitulo() {
+  var titulo = document.querySelector(".titulo");
+  titulo.style.animation =
+    "fadeOut 3s ease-in-out forwards"; /* Duración y función de temporización de la desaparición */
+  setTimeout(function () {
+    titulo.style.display = "none";
+  }, 3000); // Espera 3 segundos antes de ocultar completamente
+}
+
+// Llama a la función después de 216 segundos (216,000 milisegundos)
+setTimeout(ocultarTitulo, 216000);
